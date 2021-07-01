5983b3a8bb5d35910dd5406c7f0d338c4aa1e27c
Thiago-Mtt <thiagomotta.3000@gmail.com>
2021-06-30 22:06:36 -0300

    Adiciona classes

---
+++ sistema.py
@@ -0,0 +1,32 @@
+class Materia:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __str__(self):
+        return self.nome
+
+
+class Professor:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __str__(self):
+        return self.nome
+
+class Aluno:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __str__(self):
+        return self.nome
+
+class Turma:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __str__(self):
+        return self.nome
